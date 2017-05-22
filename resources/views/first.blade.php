b7d557112b1c727916a648622c23d01315f2e6c1
sahil0013 <sahil4cosmicneurons@gmail.com>
2017-05-22 20:16:30 +0530

    initial commit

---
+++ resources/views/first.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>My First Heading</h1>
+
+    <p>My first paragraph.</p>
+@endsection
---
+++ resources/views/photo.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <center>
+    <img src="{{asset('images/4c logo.jpg')}}">
+    </center>
+@endsection
